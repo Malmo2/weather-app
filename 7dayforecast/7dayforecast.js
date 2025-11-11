1f47387ccde02626d795204b6b55406d176d81e4
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-11 17:59:10 +0100

    7-day-forecast

---
+++ 7dayforecast/7dayforecast.js
@@ -0,0 +1,35 @@
+export function forecast(dailyData) {
+    const container = document.createElement('div');
+    container.className = "seven-day-forecast";
+
+    const title = document.createElement('h2');
+    title.textContent = "7-day forecast"
+    container.appendChild(title)
+
+    const sevenDays = dailyData.slice(0, 7);
+
+    sevenDays.forEach(day => {
+        const dayDiv = document.createElement('div');
+            dayDiv.className = "forecast-day";
+
+        const date = new Date(day.dt * 1000)  // Ett sätt att omvandla unix-tid
+         const weekday = date.toLocaleDateString("en-GB", { weekday: "short" })
+         
+         
+         const temp = day.temp.day
+         const desc = day.weather[0].description
+         const icon = day.weather[0].icon
+
+         
+         dayDiv.innerHTML = `<h3>${weekday}</h3> 
+            <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${desc}">
+            <p>${desc}</p>
+            <p>${temp}°C</p> `;
+
+            container.appendChild(dayDiv);
+         
+    })
+
+    return container;
+
+}
--- js/classes/weatherServiceC.js
+++ js/classes/weatherServiceC.js
@@ -1,8 +1,7 @@
 export class WeatherService {
     async getWeather(lat, lon) {
         try {
-            const weatherUrl =
-                `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`;
+            const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min,weathercode&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code&timezone=auto`;
 
             const res = await fetch(weatherUrl);
             if (!res.ok) throw new Error('Could not fetch data');
@@ -14,7 +13,7 @@ export class WeatherService {
             const humidity = data.current.relative_humidity_2m;
             const windSpeed = data.current.wind_speed_10m;
 
-            return { temp, humidity, windSpeed };
+            return { temp, humidity, windSpeed, daily: data.daily};
         } catch (err) {
             console.error('Could not find data', err.message);
         }
