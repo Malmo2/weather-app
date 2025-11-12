d49ab6c2caecf28c1c9b7813a0b793cacf9ed8c2
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-11-12 18:50:56 +0100

    connected html to existing js

--- maintest.js
+++ maintest.js
@@ -2,17 +2,15 @@ import { getCity } from "./js/weatherService.js";
 import { fetchForecastByCoords } from "./7dayforecast/new7dayforecast.js";
 import { forecast } from "./7dayforecast/forecastView.js";
 
-const cont = document.createElement("div");
-const displayCity = document.createElement("h2");
-const displayCountry = document.createElement("h2");
-const displayTemp = document.createElement("p");
-const displayHumid = document.createElement("p");
-const displayWindSpeed = document.createElement("p");
+const displayCity = document.getElementById('location');
+const displayTemp = document.getElementById('mainTemp');
+const displayHumid = document.getElementById("humidity");
+const displayWindSpeed = document.getElementById("windSpeed");
+const forecastContainer = document.querySelector('.center-column');
 
 const btn = document.getElementById("searchBtn");
 const cityInput = document.getElementById("searchInput");
 
-document.body.append(cont);
 
 btn.addEventListener("click", async () => {
   try {
@@ -22,24 +20,18 @@ btn.addEventListener("click", async () => {
 
     const { conditions, dailyData } = await fetchForecastByCoords(city.lat, city.lon);
 
-    displayCity.textContent = `City: ${city.city}`;
-    displayCountry.textContent = `Country: ${city.country}`;
-    displayTemp.textContent = `Temp: ${conditions.temp}°C`;
-    displayHumid.textContent = `Humidity: ${conditions.humidity}%`;
-    displayWindSpeed.textContent = `Wind speed: ${conditions.windSpeed} m/s`;
+    displayCity.textContent = `${city.city}, ${city.country}`;
+    displayTemp.textContent = `${conditions.temp}°C`;
+    displayHumid.textContent = `${conditions.humidity}%`;
+    displayWindSpeed.textContent = `${conditions.windSpeed} Km/h`;;
 
     const forecastElement = forecast(dailyData);
+    forecastContainer.innerHTML = "";
+    forecastContainer.appendChild(forecastElement);
 
-    cont.innerHTML = "";
-    cont.append(
-      displayCity,
-      displayCountry,
-      displayTemp,
-      displayHumid,
-      displayWindSpeed,
-      forecastElement
-    );
+
   } catch (err) {
     console.error("Could not fetch data", err.message);
   }
 });
+
