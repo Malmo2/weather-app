5e334766e390fe98c1459fc664008daf6222169c
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-24 10:59:34 +0100

    hourly-forecast

--- Animation/weatherByTemp.js
+++
@@ -1,8 +0,0 @@
-// weatherByTemp.js
-
-export function getWeatherByTemp(temp) {
-    if (temp > 18) return "summer";
-    if (temp > 10) return "spring";
-    if (temp > 2)  return "autumn";
-    return "winter";
-}
