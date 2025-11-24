aee068a4271ccb8f8748d8a58629ba5d65c6a006
meskilsson <mattiaseskilsson@hotmail.se>
2025-11-24 11:00:55 +0100

    classes and rendering for precipitation and feels like

--- newtest.js
+++ newtest.js
@@ -2,6 +2,10 @@ import { extractGeo } from "./newjs/classes/cityClass.js";
 import { createCities } from "./newjs/rendering/cities.js";
 import { createSolarData } from "./newjs/rendering/solar.js";
 import { solarData } from "./newjs/classes/solarData.js";
+import { precipitationData } from "./newjs/classes/precipitationData.js";
+import { createPrecipitation } from "./newjs/rendering/precipitation.js";
+import { feelsLikeData } from "./newjs/classes/feelslikeData.js";
+import { createFeelslike } from "./newjs/rendering/feelslike.js";
 
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
@@ -18,8 +22,13 @@ searchBtn.addEventListener('click', async () => {
     const search = searchInput.value.trim();
     const cities = await extractGeo(search);
     const sol = await solarData();
+    const prec = await precipitationData();
+    const feels = await feelsLikeData();
 
 
     createCities(cities);
     createSolarData(sol);
+    createPrecipitation(prec);
+    createFeelslike(feels)
+
 });
