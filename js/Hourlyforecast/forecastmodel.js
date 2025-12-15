edbb67082e2c6a6c37667ac28c48f0718d449c21
Emelie Björkman <158598324+bjorkmanemelie@users.noreply.github.com>
2025-12-15 16:12:53 +0100

    Revert "Feature/weather service test"

--- js/Hourlyforecast/forecastmodel.js
+++ js/Hourlyforecast/forecastmodel.js
@@ -1,11 +1,8 @@
-import { getWeatherIcon } from "../7dayforecast/7dayforecast.js";
-
 export class Forecastmodel {
 
-    static getNextHours(get, count = 11 ) {
+    static getNextHours(get, count = 12 ) {
         const times = get.hourly.time;
         const temps = get.hourly.temperature_2m;
-        const codes = get.hourly.weather_code ?? [];
 
         const now = Date.now()
         const result = [];
@@ -13,13 +10,9 @@ export class Forecastmodel {
         for(let i = 0; i < times.length && result.length < count; i++) {
             const d = new Date(times[i])
             if(d >= now) {
-                const code = codes[i];
-                const { icon, label } = getWeatherIcon(code);
                 result.push({
                     date: d,
-                    temp: temps[i],
-                    icon,
-                    iconLabel: label
+                    temp: temps[i]
                 })
             }
         }
